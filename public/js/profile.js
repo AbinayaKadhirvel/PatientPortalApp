094c1d80a621bfcd1d4bcfcf51749f82eb89aea9
AbinayaKadhirvel <abi.psg@gmail.com>
2018-05-07 09:34:46 +0530

    Update profile.js

--- public/js/profile.js
+++ public/js/profile.js
@@ -1,6 +1,6 @@
 jQuery(document).ready(function(){
     jQuery('#edit-profile').on('submit', function() {
-            alert('gee');
+          
     });
     jQuery('.btn-danger').on('click', function() {
 
